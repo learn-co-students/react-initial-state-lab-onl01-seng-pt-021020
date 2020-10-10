800c5d066308eec25dfa7120faf2c267394f7a11
08fdange <08fdange@gmail.com>
2020-10-09 22:10:10 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,34 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    handleTimer = () => {
+        if (this.props.initialCount < 0) {
+            setTimeout(() => {
+                this.setState({ 
+                    secondsLeft: (this.props.initialCount - 1)
+                })
+            }, 1000)
+            return this.state.secondsLeft
+        }
+        else {
+            const div = document.querySelector('.seconds-left')
+            div.innerText = "Boom!"
+        }
+    }
+
+    render() {
+        const timer = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+        return(
+            <div>{timer}</div>
+        )
+    }
+}
